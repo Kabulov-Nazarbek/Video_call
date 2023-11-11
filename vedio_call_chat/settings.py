5daa6133f4551efe87082dcabcdb965e168bfb24
Kabulov-Nazarbek <nazarbekqobulov28@gmail.com>
2023-11-11 12:38:29 +0500

    bugs fixed

--- vedio_call_chat/settings.py
+++ vedio_call_chat/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u6l(-$_cln!ff&nfiy)m-jq3(oa8er(=)rruy8orgg#bmd8k(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
